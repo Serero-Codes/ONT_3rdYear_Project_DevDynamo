ef9dd9c22fca50613ac57c34185f13fb19a17e82
DevDynamo-cs <neohselala04@gmail.com>
2025-08-06 22:39:14 +0200

    added a method in wardadamin controller

--- Controllers/WardAdminController.cs
+++ Controllers/WardAdminController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ONT_3rdyear_Project.Data;
+using ONT_3rdyear_Project.Models;
 
 namespace ONT_3rdyear_Project.Controllers
 {
     public class WardAdminController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public WardAdminController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        // Search for patients
+        public async Task<IActionResult> Search(string query)
+        {
+            if (string.IsNullOrWhiteSpace(query))
+            {
+                return View(new List<Patient>());
+            }
+
+            var patients = await _context.Patients
+                .Where(p => p.FirstName.Contains(query) ||
+                           p.LastName.Contains(query))
+                           //p.IDNumber.Contains(query))
+                .Include(p => p.Admissions)
+                .ThenInclude(a => a.Bed)
+                .ToListAsync();
+
+            return View(patients);
         }
     }
 }
